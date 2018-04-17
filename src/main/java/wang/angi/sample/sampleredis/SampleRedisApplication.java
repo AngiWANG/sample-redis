b299bcf8cb17b3ca48a513b466d81bed30c682e6
wanglei <wanglei@koolyun.com>
2018-04-17 17:33:49 +0800

    init

---
+++ src/main/java/wang/angi/sample/sampleredis/SampleRedisApplication.java
@@ -0,0 +1,60 @@
+package wang.angi.sample.sampleredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @author angi
+ */
+@SpringBootApplication
+public class SampleRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleRedisApplication.class, args);
+    }
+
+    /**
+     * LettuceConnectionFactory
+     *
+     * @return
+     */
+    @Bean
+    public RedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("localhost");
+        redisStandaloneConfiguration.setPort(6379);
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration);
+        return jedisConnectionFactory;
+    }
+
+    /**
+     * RedisTemplate
+     *
+     * @return
+     */
+    @Bean
+    RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+
+    /**
+     * StringRedisTemplate
+     *
+     * @return
+     */
+    @Bean
+    StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return stringRedisTemplate;
+    }
+}
---
+++ src/test/java/wang/angi/sample/sampleredis/SampleRedisApplicationTests.java
@@ -0,0 +1,52 @@
+package wang.angi.sample.sampleredis;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleRedisApplicationTests {
+
+    // inject the actual template
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    // inject the template as valueOperations
+    @Resource(name = "redisTemplate")
+    private ValueOperations<String, String> valueOps;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testRedisTemplate() {
+        redisTemplate.opsForValue().set("key1", "value1");
+        Assert.assertEquals("value1", redisTemplate.opsForValue().get("key1"));
+    }
+
+    @Test
+    public void testValueOperations() {
+        valueOps.set("key2", "value2");
+        Assert.assertEquals("value2", valueOps.get("key2"));
+    }
+
+    @Test
+    public void testStringRedisTemplate() {
+        stringRedisTemplate.opsForValue().set("key1", "value1");
+        Assert.assertEquals("value1", stringRedisTemplate.opsForValue().get("key1"));
+    }
+}
